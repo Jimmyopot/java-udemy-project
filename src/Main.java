c7a584549dceb39a1965a84fce037c0539b75642
Jimmy Opot <jimmyopot@gmail.com>
2024-06-13 09:02:37 +0300

    Working on autoboxing in Working on Banking application with Linked lists and inheritance

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+    }
+
+    public class Branch {
+        // write code here
+        private String name;
+        private ArrayList<Customer> customers;
+
+        public Branch(String name) {
+            this.name = name;
+            this.customers = new ArrayList<Customer>();
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public ArrayList<Customer> getCustomers() {
+            return customers;
+        }
+
+
     }
 }
